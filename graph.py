427cfc7d5891a212f2d04f78ed1003769960eb9a
Vivek Verma <vivnps.verma@gmail.com>
2018-11-06 16:05:37 -0800

    Initial Commit

---
+++ graph.py
@@ -0,0 +1,5 @@
+import matplotlib.pyplot as plt
+def plot(fx, fy, boundx=[-10,10], boundy=[-10,10]):
+    plt.axes()
+    plt.arrow(0, 0, 0.5, 0.5, head_width=0, head_length=0, fc='k', ec='k')
+    plt.show()
