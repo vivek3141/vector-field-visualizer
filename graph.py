347a492e9116f0e1dd3c0f38befa475da946021b
Vivek Verma <vivnps.verma@gmail.com>
2018-11-07 19:29:56 -0800

    Basic Vector field

--- graph.py
+++ graph.py
@@ -1,7 +1,22 @@
 import matplotlib.pyplot as plt
 
 
-def plot(fx, fy, boundx=[-10, 10], boundy=[-10, 10]):
-    plt.axes()
-    plt.arrow(0, 0, 0.5, 0.5, head_width=0, head_length=0, fc='k', ec='k')
+def plot(fx, fy, boundx=[-10, 10], boundy=[-10, 10], skip=1):
+    for y in range(boundy[0] - 1, boundy[1] + 1, skip):
+        for x in range(boundx[0] - 1, boundy[1] + 1, skip):
+            print(x, y)
+            plt.scatter([x], [y], c='black', s=[10])
+            plt.arrow(x, y, fx(x, y), fy(x, y),
+                      head_width=0.5, head_length=0.5, fc='k', ec='k')
     plt.show()
+
+
+def Fx(x, y):
+    return 1
+
+
+def Fy(x, y):
+    return 1
+
+
+plot(Fx, Fy, skip=2)
