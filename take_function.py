1cbd962cf31fa786fc0deaa3770d2b1d3e0bf8e9
Vivek Verma <vivnps.verma@gmail.com>
2018-11-08 14:41:55 -0800

    Adding Functions for Color and Length

--- take_function.py
+++ take_function.py
@@ -1,4 +1,14 @@
 from math import *
 import graph as g
-def graph(x_func, y_func):
-    fx = eval("lambda x: "+str(x_func))
+
+
+def graph(x_func, y_func, bound_x=(-10, 10), bound_y=(-10, 10), skip=1):
+    fx = eval("lambda x: " + str(x_func))
+    fy = eval("lambda y: " + str(y_func))
+    g.plot(fx, fy, bound_x=bound_x, bound_y=bound_y, skip=skip)
+
+
+def graph_color(x_func, y_func, bound_x=(-10, 10), bound_y=(-10, 10), skip=1, prop=0):
+    fx = eval("lambda x: " + str(x_func))
+    fy = eval("lambda y: " + str(y_func))
+    g.plot_color(fx, fy, bound_x=bound_x, bound_y=bound_y, skip=skip, prop=prop)
