df420aaa620cf63532aef8bdc4fef77c9d3e249e
Vivek Verma <vivnps.verma@gmail.com>
2018-11-07 22:00:52 -0800

    Adding Color

--- graph.py
+++ graph.py
@@ -1,22 +1,41 @@
 import matplotlib.pyplot as plt
+import math
+
+color_list = ['#e22b2b', '#e88e10', '#eae600', '#88ea00',
+              '#00eae2', '#0094ea', "#2700ea", '#bf00ea', '#ea0078']
 
 
-def plot(fx, fy, boundx=[-10, 10], boundy=[-10, 10], skip=1):
-    for y in range(boundy[0] - 1, boundy[1] + 1, skip):
-        for x in range(boundx[0] - 1, boundy[1] + 1, skip):
-            print(x, y)
-            plt.scatter([x], [y], c='black', s=[10])
+def plot(fx, fy, bound_x=(-10, 10), bound_y=(-10, 10), skip=1):
+    for y in range(bound_y[0] - 1, bound_y[1] + 1, skip):
+        for x in range(bound_x[0] - 1, bound_y[1] + 1, skip):
+            c = "#0F0F0F"
+            plt.scatter([x], [y], c=c, s=[10])
             plt.arrow(x, y, fx(x, y), fy(x, y),
-                      head_width=0.5, head_length=0.5, fc='k', ec='k')
+                      head_width=0.5, head_length=0.5, color=c)
     plt.show()
 
 
-def Fx(x, y):
-    return 1
+def plot_color(fx, fy, bound_x=(-10, 10), bound_y=(-10, 10), skip=1, prop=0):
+    for y in range(bound_y[0] - 1, bound_y[1] + 1, skip):
+        for x in range(bound_x[0] - 1, bound_y[1] + 1, skip):
+            v = int(math.sqrt(x ** 2 + y ** 2) / 10 ** prop)
+            index = len(color_list)-1 if v > len(color_list)-1 else v
+            c = color_list[index]
+            plt.scatter([x], [y], c=c, s=[10])
+            x_val = fx(x, y)
+            y_val = fy(x, y)
+            angle = math.atan(y_val/x_val) if x_val < 0 else math.atan(y_val/x_val) + math.pi
+            plt.arrow(x, y, x_val, y_val,
+                      head_width=0.5, head_length=0.5, color=c)
+    plt.show()
 
 
-def Fy(x, y):
-    return 1
+def f_x(x, y):
+    return x
 
 
-plot(Fx, Fy, skip=2)
+def f_y(x, y):
+    return y
+
+
+plot_color(f_x, f_y, skip=2)
