f18d33ae4dbf81dba2c7eb04cdfcf7ff68005913
Vivek Verma <vivnps.verma@gmail.com>
2018-11-10 19:26:00 -0800

    Adding Flask app

--- src/main.py
+++ src/main.py
@@ -1,7 +1,17 @@
 from flask import Flask
+import take_function as t
+from flask import request
+from flask import send_file
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello from Flask!'
+
+@app.route('/color')
+def get_graph():
+    fx = request.args.get('fx')
+    fy = request.args.get('fy')
+    t.graph_color(fx,fy, skip=2)
+    return send_file("./vector_field.jpg")
+
+
+app.run()
