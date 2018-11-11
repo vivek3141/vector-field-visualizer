f15f3c3bf203c9feb8c9395b51a1224488c4da9f
Vivek Verma <vivnps.verma@gmail.com>
2018-11-10 20:23:28 -0800

    Adding Main File

--- main.py
+++ main.py
@@ -4,18 +4,18 @@ import take_function as t
 parser = argparse.ArgumentParser(description='Run the program')
 parser.add_argument('fx', metavar='fx', type=str, help='X Function')
 parser.add_argument('fy', metavar='fy', type=str, help='Y Function')
-parser.add_argument('mode', metavar='mode', type=str,
+parser.add_argument('--mode', metavar='mode', type=str,
                     help="Specify 'color' or 'black' to choose whether to use actual length or color", default="color",
                     nargs='?')
-parser.add_argument('skip', metavar='skip', type=int, help='Number of units to skip before each vector', default=2,
+parser.add_argument('--skip', metavar='skip', type=int, help='Number of units to skip before each vector', default=2,
                     nargs='?')
-parser.add_argument('boundx', metavar='boundx', type=str,
+parser.add_argument('--boundx', metavar='boundx', type=str,
                     help='Number of X values to show. Eg. -10,10', nargs='?', default="-10,10")
-parser.add_argument('boundy', metavar='boundy', type=str, help='Number of X values to show. Eg. -10,10',
+parser.add_argument('--boundy', metavar='boundy', type=str, help='Number of X values to show. Eg. -10,10',
                     default='-10,10', nargs='?')
-parser.add_argument('prop', metavar='prop', type=int, help='Set this value to change the cutoff for changing color',
+parser.add_argument('--prop', metavar='prop', type=int, help='Set this value to change the cutoff for changing color',
                     default=0, nargs='?')
-parser.add_argument('head_size', metavar='head_size', type=float, help='Size of arrow heads',
+parser.add_argument('--head_size', metavar='head_size', type=float, help='Size of arrow heads',
                     default=0.5, nargs='?')
 
 args = parser.parse_args()
