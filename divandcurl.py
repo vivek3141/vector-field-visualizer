5aa377dbb61d0f1b78e179867b150993278a9ba6
Vivek Verma <vivnps.verma@gmail.com>
2018-11-16 10:53:58 -0800

    Formatting

--- divandcurl.py
+++ divandcurl.py
@@ -11,6 +11,3 @@ def curl(fx, fy, x, y, d=0.0001):
     f_x = eval("lambda x,y: " + str(fx))
     f_y = eval("lambda x,y: " + str(fy))
     return round(((f_y(x + d, y) - f_y(x, y)) / d) - ((f_x(x, y + d) - f_x(x, y)) / d), 3)
-
-
-print(curl("x*y+1", "1+y", 2, 1))
