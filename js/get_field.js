46c2bd65b4f0b96b266d42bf58bc7fe6797252a8
Vivek Verma <vivnps.verma@gmail.com>
2018-11-20 13:04:00 -0800

    Fixing JS

--- js/get_field.js
+++ js/get_field.js
@@ -2,7 +2,7 @@ const button = document.getElementById("graph");
 button.addEventListener("click", e => {
     const fx = document.getElementById("Fx").value;
     const fy = document.getElementById("Fy").value;
-    const skip = 1;
+    const skip = 2;
     const url = "https://vector-field.herokuapp.com/?fx=" + fx + "&fy=" + fy + "&skip=" + skip;
     document.getElementById("loading").innerHTML = "Loading ...";
     document.getElementById("img").src = url;
