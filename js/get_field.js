da991dc41927fe34df949da7f59c1781842fb0d2
Vivek Verma <vivnps.verma@gmail.com>
2018-11-21 18:07:10 -0800

    Trying Blank

--- js/get_field.js
+++ js/get_field.js
@@ -1,10 +1,9 @@
 const button = document.getElementById("graph");
 button.addEventListener("click", e => {
-    document.getElementById("img").src = "./img/loading.jpg";
+    document.getElementById("img").src = "";
     const fx = document.getElementById("Fx").value;
     const fy = document.getElementById("Fy").value;
     const skip = 2;
     const url = "https://vector-field.herokuapp.com/?fx=" + fx + "&fy=" + fy + "&skip=" + skip;
-    document.getElementById("img").src="./img/loading.jpg"
     document.getElementById("img").src = url;
 });
