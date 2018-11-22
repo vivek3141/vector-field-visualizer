f3a2f8a3821f6ee7f54dcca6a7b29a33b55764b6
Vivek Verma <vivnps.verma@gmail.com>
2018-11-21 18:06:51 -0800

    Trying Loading Screen

--- js/get_field.js
+++ js/get_field.js
@@ -3,7 +3,8 @@ button.addEventListener("click", e => {
     document.getElementById("img").src = "./img/loading.jpg";
     const fx = document.getElementById("Fx").value;
     const fy = document.getElementById("Fy").value;
-    const skip = 1;
+    const skip = 2;
     const url = "https://vector-field.herokuapp.com/?fx=" + fx + "&fy=" + fy + "&skip=" + skip;
+    document.getElementById("img").src="./img/loading.jpg"
     document.getElementById("img").src = url;
 });
