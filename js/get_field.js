45c90ff55b8132678f670367daa800bfd167fdf6
Vivek Verma <vivnps.verma@gmail.com>
2018-11-21 18:06:10 -0800

    Adjusting Skip

--- js/get_field.js
+++ js/get_field.js
@@ -1,11 +1,9 @@
 const button = document.getElementById("graph");
 button.addEventListener("click", e => {
+    document.getElementById("img").src = "./img/loading.jpg";
     const fx = document.getElementById("Fx").value;
     const fy = document.getElementById("Fy").value;
-    const skip = 2;
+    const skip = 1;
     const url = "https://vector-field.herokuapp.com/?fx=" + fx + "&fy=" + fy + "&skip=" + skip;
-    document.getElementById("loading").innerHTML = "Loading ...";
     document.getElementById("img").src = url;
-    document.getElementById("loading").innerHTML = "";
-
 });
