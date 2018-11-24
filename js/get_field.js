607b76883299de184c34090f73db7b6b87b1f72a
Vivek Verma <vivnps.verma@gmail.com>
2018-11-23 18:11:44 -0800

    Debugging

--- js/get_field.js
+++ js/get_field.js
@@ -5,5 +5,6 @@ button.addEventListener("click", e => {
     const fy = document.getElementById("Fy").value;
     const skip = 2;
     const url = "https://vector-field.herokuapp.com/?fx=" + fx + "&fy=" + fy + "&skip=" + skip;
+    console.log(url);
     document.getElementById("img").src = url;
 });
