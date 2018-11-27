3f12e691ba2d1e2ab19446709506214334bfa091
Vivek Verma <vivnps.verma@gmail.com>
2018-11-26 18:45:53 -0800

    Updating API and adding fields

--- js/get_field.js
+++ js/get_field.js
@@ -3,7 +3,8 @@ button.addEventListener("click", e => {
     document.getElementById("img").src = "";
     const fx = document.getElementById("Fx").value;
     const fy = document.getElementById("Fy").value;
-    const skip = 2;
+    const skip = document.getElementById("skip").value;
+    const bounds = document.getElementById("bounds").value;
     const url = "https://vector-field.herokuapp.com/?fx=" + fx + "&fy=" + fy + "&skip=" + skip;
     console.log(url);
     document.getElementById("img").src = url;
