979c755dc617678c7914eab66f607eb6bb9500e9
Vivek Verma <vivnps.verma@gmail.com>
2019-01-25 21:09:58 -0800

    Adding MUCH faster server

--- js/get_field.js
+++ js/get_field.js
@@ -23,7 +23,7 @@ button.addEventListener("click", e => {
     const fy = document.getElementById("Fy").value.replace("+","@");
     const skip = document.getElementById("skip").value;
     const bounds = document.getElementById("bounds").value;
-    const url = "https://vector-field.herokuapp.com/?fx=" + fx + "&fy=" + fy + "&skip=" + skip + "&bounds=" + bounds;
+    const url = "https://vectorfield.pythonanywhere.com//?fx=" + fx + "&fy=" + fy + "&skip=" + skip + "&bounds=" + bounds;
     console.log(url);
     document.getElementById("img").src = url;
 });
