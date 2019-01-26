1063dc77cb519cdb20d47175c65f246e6c44cd53
Vivek Verma <vivnps.verma@gmail.com>
2019-01-25 21:29:20 -0800

    Fixing main.py

--- main.py
+++ main.py
@@ -9,23 +9,15 @@ parser.add_argument('--mode', metavar='mode', type=str,
                     nargs='?')
 parser.add_argument('--skip', metavar='skip', type=int, help='Number of units to skip before each vector', default=1,
                     nargs='?')
-parser.add_argument('--boundx', metavar='boundx', type=str,
-                    help='Number of X values to show. Eg. -10,10', nargs='?', default="-10,10")
-parser.add_argument('--boundy', metavar='boundy', type=str, help='Number of X values to show. Eg. -10,10',
-                    default='-10,10', nargs='?')
+parser.add_argument('--bound', metavar='bound', type=str,
+                    help='Number of values to show. Eg. -10,10', nargs='?', default="-10,10")
 parser.add_argument('--prop', metavar='prop', type=int, help='Set this value to change the cutoff for changing color',
                     default=0, nargs='?')
-parser.add_argument('--head_size', metavar='head_size', type=float, help='Size of arrow heads',
-                    default=0.5, nargs='?')
 
 args = parser.parse_args()
 if args.mode.upper() == "COLOR":
     v = Visualizer(f_x=args.fx, f_y=args.fy)
-    v.plot_color(bound_x=tuple(map(int, args.boundx.split(','))),
-                 bound_y=tuple(map(int, args.boundy.split(','))), skip=args.skip, prop=args.prop,
-                 head_size=args.head_size)
+    v.plot_color(bound=tuple(map(int, args.bound.split(','))), skip=args.skip, prop=args.prop)
 elif args.mode.upper() == "BLACK":
     v = Visualizer(f_x=args.fx, f_y=args.fy)
-    v.plot(bound_x=tuple(map(int, args.boundx.split(','))),
-           bound_y=tuple(map(int, args.boundy.split(','))), skip=args.skip,
-           head_size=args.head_size)
+    v.plot(bound=tuple(map(int, args.boundx.split(','))), skip=args.skip,)
